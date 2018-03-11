7e213032cf05efd536c0e43ba4fc624c480796aa
yanjw <dupouy2008@gmail.com>
2018-03-12 00:33:07 +0800

    机械臂处理卸货逻辑

--- trunk/Assets/Scripts/core/Arm.cs
+++ trunk/Assets/Scripts/core/Arm.cs
@@ -6,7 +6,12 @@ public class Arm : MonoBehaviour {
     List<Collider> colliders = new List<Collider>();
 
     Animator animator;
+
     int IDLE_HASH;
+    int RUN_HASH;
+
+    int HAS_CARGO_ENUM;
+    bool hasCargo = false;
 
     Transform arm;
 
@@ -14,17 +19,25 @@ public class Arm : MonoBehaviour {
 	void Start () {
         animator = GetComponent<Animator>();
         IDLE_HASH = Animator.StringToHash("Idle");
+        RUN_HASH = Animator.StringToHash("Run");
+        HAS_CARGO_ENUM = Animator.StringToHash("hasCargo");
         arm = transform.GetChild(0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash == IDLE_HASH && colliders.Count > 0) {
+
+    // Update is called once per frame
+    void Update() {
+        if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash == IDLE_HASH && colliders.Count > 0 && !hasCargo) {
             Collider c = colliders[0];
-            //c.transform.SetParent(arm);
-            animator.Play(Animator.StringToHash("Run"));
-            colliders.Remove(c);
+            c.transform.SetParent(arm);
+            hasCargo = true;
         }
+        else if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash == RUN_HASH && hasCargo) {
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f) {
+                hasCargo = false;
+                arm.GetChild(0).transform.SetParent(null);
+            }
+        }
+            animator.SetBool(HAS_CARGO_ENUM, hasCargo);
 	}
 
     private void OnTriggerEnter(Collider other) {
