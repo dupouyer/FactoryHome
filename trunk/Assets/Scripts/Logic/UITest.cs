7cf07bb686a3b9f87032399331f00de2abb4bd52
yanjw <dupouy2008@gmail.com>
2018-03-16 00:06:12 +0800

    UI 框架添加

---
+++ trunk/Assets/Scripts/Logic/UITest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class UITest : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake() {
+        UIPackage.AddPackage("UI/Common");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ trunk/Assets/Scripts/UI/BuildPanel/BuildPanelBinder.cs
@@ -0,0 +1,14 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+
+namespace BuildPanel
+{
+	public class BuildPanelBinder
+	{
+		public static void BindAll()
+		{
+			UIObjectFactory.SetPackageItemExtension(UI_Panel.URL, typeof(UI_Panel));
+		}
+	}
+}
